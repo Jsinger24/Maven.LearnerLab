dff34436e8d04a0e472f337342689aeff5b86364
jd <j.judd.singer@gmail>
2022-07-20 17:24:27 -0400

    passed constructor test

---
+++ src/main/java/io/zipcoder/interfaces/Learner.java
@@ -0,0 +1,14 @@
+package io.zipcoder.interfaces;
+
+public interface Learner {
+
+    public static void learn(double numberOfHours){
+
+    }
+
+    public static  getTotalStudyTime(){
+        return double;
+    }
+
+
+}
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,20 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+    @Test
+    public void testConstructor(){
+
+        String expectedName = "Arnold";
+        long expectedId = 69;
+
+        Person test = new Person(expectedName, expectedId);
+
+        Assert.assertEquals(expectedName, test.getName());
+        Assert.assertEquals(expectedId, test.getId());
+    }
+
 
 }
