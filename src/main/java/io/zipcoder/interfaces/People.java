33823230c77b0288b2263c2538251464f617fc67
jd <j.judd.singer@gmail>
2022-07-27 09:45:33 -0400

    trying to do tests in testPeople

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,13 +1,12 @@
 package io.zipcoder.interfaces;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class People {
 
     List<Person> personList = new ArrayList<>();
-
     public void add(Person person){
         this.personList.add(person);
     }
@@ -20,4 +19,59 @@ public class People {
         }
         return null;
     }
+
+    public Boolean contains(Person person){
+        if(personList.contains(person)){
+            return true;
+        }
+        return null;
+    }
+
+    public void remove(Person person){
+        for (Person holder: personList){
+            if (person == holder){
+                personList.remove(holder);
+            }
+        }
+
+}
+
+    public void remove(long id){
+        for (Person holder: personList) {
+            if(holder.getId() ==  id){
+                personList.remove(holder);
+        }
+        }
+    }
+
+    public void removeAll(){
+        personList.clear();
+    }
+
+    public int count(){
+        return personList.size();
+    }
+
+    public void toArray(){
+        personList.toArray();
+    }
+
+    public void iterator(){
+//        Iterator<E> iterator = new Iterator<E>() {
+//            @Override
+//            public boolean hasNext() {
+//                return false;
+//            }
+//
+//            @Override
+//            public E next() {
+//                return null;
+//            }
+//        }
+
+    }
+
+
+
+
 }
---
+++ src/test/java/io/zipcoder/interfaces/TestPeople.java
@@ -0,0 +1,16 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Test;
+
+public class TestPeople {
+
+    @Test
+    public void testAdd(){
+
+        Person person = new Person("Wolverine", 1234);
+
+
+
+
+    }
+}
